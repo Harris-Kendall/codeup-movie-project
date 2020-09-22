dc9ce911c96d03d01c57147c2ef0dd0d03ec07d9
Donelle Harris <donelle.harris@gmail.com>
2020-09-22 12:35:15 -0500

    added TODOs

---
+++ codeup-movie-project.js
@@ -0,0 +1,8 @@
+/*
+TODO
+    - display "loading..." message
+    - make AJAX request to get a listing of all movies
+    - remove the "loading..." message & replace with HTML generated from the json response
+    -create a form for adding a new movie {name, title, rating}
+
+ */
